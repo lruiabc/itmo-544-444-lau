f22315acd057dd96aa72d0a7281bf065328fbc9e
lruiabc <568047669@qq.com>
2015-10-25 02:58:19 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name itmo544-spring-virtualbox --security-group-ids $4 --subnet-id $5 --associate-public-ip-address --user-data file://itmo-544-444-env/install-env.sh --user-data file://itmo-544-444-app/install-ap.sh --debug
+aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name itmo544-spring-virtualbox --security-group-ids $4 --subnet-id $5 --associate-public-ip-address --user-data file://itmo-544-444-env/install-env.sh --user-data file://itmo-544-444-app/install-app.sh --debug
 
 
